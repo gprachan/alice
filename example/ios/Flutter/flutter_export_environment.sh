80986d38fb1733f51aa6ea1708db627c7bc2afd5
Prachan Ghale <70263211+ghaleprachan@users.noreply.github.com>
2023-07-29 17:38:07 +0545

    Update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\libs\alice\example"
+export "FLUTTER_ROOT=/Users/prachanghale/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/prachanghale/workspace/libs/alice/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
